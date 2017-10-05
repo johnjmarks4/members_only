af0e8fb592705a41ffa43d4f39c945bc2b03a4fb
John Marks <johnjmarks4@gmail.com>
2017-10-05 15:41:10 -0500

    Add password authentification to sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id 
+      session[:user_id] = @user.id
+      session[:password] = params[:user][:password]
       redirect_to sessions_create_path
     else
-      # Would normally be new path, but we aren't actually creating new users
-      redirect_to sessions_create_path
+      redirect_to sessions_new_path
     end
   end
 
