10df26b1815fe4f079ca4354c4987006a8d29b7b
John Marks <johnjmarks4@gmail.com>
2017-10-05 15:31:20 -0500

    Fill out sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(id: session[:user_id])
+    if !@user.nil?
+      redirect_to sessions_new_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
+require 'bcrypt'
+require 'digest'
+
 class UsersController < ApplicationController
   def new
   end
 
   def create
-    @user = User.new(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+    @user = User.new(user_params)
     if @user.save
-      #
+      session[:user_id] = @user.id 
+      redirect_to sessions_create_path
+    else
+      # Would normally be new path, but we aren't actually creating new users
+      redirect_to sessions_create_path
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
